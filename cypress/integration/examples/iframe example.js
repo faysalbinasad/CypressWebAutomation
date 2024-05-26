3e36c978ca5552df69872af5096cc96d9c78aebc
faysalbinasad <faysalbinasad3334@gmail.com>
2024-05-26 12:20:52 +0600

    Update

--- cypress/integration/examples/iframe example.js
+++ cypress/integration/examples/iframe example.js
@@ -12,7 +12,7 @@ describe("My iframe test suite", function(){
 
         cy.iframe().find("a[href*='mentorship']").eq(0).click()
 
-        //cy.iframe().find("h1[class*='pricing-title']").should('have.length',2)
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length',2)
 
     })
 })
