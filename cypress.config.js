9d1111d40e5cffd692ab69d35ebce739aae0a9de
faysalbinasad <faysalbinasad3334@gmail.com>
2024-05-30 16:46:51 +0600

    Changes in Config (Section:15-72)

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,24 @@
 
 const { defineConfig } = require("cypress");
 
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", preprocessor(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 module.exports = defineConfig({
 
   defaultCommandTimeout: 10000, // increasing or altering default command time (for all test cases)
@@ -21,22 +39,12 @@ module.exports = defineConfig({
   projectId: "hwp8xr", //(Project Name: CypressWebAutomation)
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
+      setupNodeEvents,
      //specPattern: 'cypress\\integration\\examples\\*.js'
+
        specPattern: 'cypress/integration/examples/**/*.js',
+
+       specPattern: 'cypress/integration/examples/BDD/**/*.feature'
   },
-
-
-/*     video: true,
-    reporter: 'mochawesome',
-    reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true
-  }, */
 
 });
