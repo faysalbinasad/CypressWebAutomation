9e62ad5c9dc47995e7c0020445f38efd5de42fa8
Al-Faysal-Bin-Asad <you@example.com>
2024-06-04 23:45:54 +0600

    Update excelDemoUpdate

--- ExcelJSUtil/excelDemoUpdate.js
+++ ExcelJSUtil/excelDemoUpdate.js
@@ -1,38 +1,55 @@
 const ExcelJS = require('exceljs');
 
-async function excelTest(){
+async function writeExcelTest(searchText, replacedText, change, filePath)
+
+{
+
 
 const Workbook = new ExcelJS.Workbook();
 
 //reading the values from the cells
 
-await Workbook.xlsx.readFile('/Users/Faysal/Downloads/ExcelDownloadTest.xlsx')
+await Workbook.xlsx.readFile(filePath)
 
 const worksheet1 = Workbook.getWorksheet('Sheet1');
 
-worksheet1.eachRow( (row, rowNumber) => 
-    {
+const output = await readExcel(worksheet1, searchText);
 
-    row.eachCell( (cell, colNumber) =>
-    {
-
-            if(cell.value === 'Apple')
-                {
-
-                    console.log(rowNumber);
-                    console.log(colNumber);
-                }
-
-    });
-
-});
-
-const cellValue = worksheet1.getCell(3,2);
-cellValue.value = "iPhone"
+const cellValue = worksheet1.getCell(output.row, output.column+change.columnChange);
+cellValue.value = replacedText;
 
 //Writing the new value to the cells
-await Workbook.xlsx.writeFile('/Users/Faysal/Downloads/ExcelDownloadTest.xlsx')
+await Workbook.xlsx.writeFile(filePath)
 
 }
 
-excelTest();
+async function readExcel(worksheet1, searchText)
+{
+    let output = {row:-1, column:-1};
+
+    worksheet1.eachRow( (row, rowNumber) => 
+        {
+    
+        row.eachCell( (cell, colNumber) =>
+        {
+    
+                if(cell.value === searchText)
+                    {
+    
+                      /*   console.log(rowNumber);
+                        console.log(colNumber); */
+    
+                        output.row = rowNumber;
+                        output.column = colNumber;
+    
+                    }
+    
+        });
+    
+    });
+    return output;
+}
+
+//Scenario: Update Mango price to 350
+
+writeExcelTest("Mango", 350, {rowChange: 0, columnChange: 2}, "/Users/Asus/Downloads/ExcelTest.xlsx");
